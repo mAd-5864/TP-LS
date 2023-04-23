7b3944119e7dcd466cff3bfafb37b40676725ec7
Pedro Amorim <pepas52@gmail.com>
2023-04-23 19:51:37 +0100

    created reset button

---
+++ jogo-galo-react/src/Components/ResetBoard.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+import "./ResetBoard.css"
+
+export const ResetBoard = ({ resetBoard }) => {
+    return (
+        <button className="reset-btn" onClick={resetBoard}>RESET</button>
+    )
+}
